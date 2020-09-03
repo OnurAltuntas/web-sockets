a8e2d8d157153e4f6b3a65e73552a082c2a69c90
Onur Altuntas <onuraltuntas50@gmail.com>
2020-09-03 19:34:38 +0300

    socket.io emit

--- server/index.js
+++ server/index.js
@@ -2,24 +2,52 @@ const express = require("express");
 const http = require("http");
 const socketIo = require("socket.io");
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 
-
-
-const router = require('./router');
+const router = require("./router");
 const app = express();
 const server = http.createServer(app);
 
 const io = socketIo(server); // < Interesting!
 
-io.on('connection',(socket)=>{
-	console.log('We have a new connection');
+/* io.on("connection", (socket) => {
+  console.log("We have a new connection");
 
-	socket.on('disconnect', () =>{
-		console.log('user just had left');
-	})
-})
+  socket.on("disconnect", () => {
+    console.log("user just had left");
+  });
+}); */
+
+var position = {
+	x : 200,
+	y : 200
+}
+
+io.on("connection", (socket) => {
+  socket.emit("position", position);
+  socket.on("move", (data) => {
+	  console.log("yazdir" + data);
+    switch (data) {
+      case "left":
+        position.x -=5;
+        io.emit("position", position);
+        break;
+      case "right":
+        position.x += 5;
+        io.emit("position", position);
+        break;
+      case "up":
+        position.y -= 5;
+        io.emit("position", position);
+        break;
+      case "down":
+        position.y += 5;
+        io.emit("position", position);
+        break;
+    }
+  });
+});
 
 app.use(router);
 
-server.listen(PORT,() => console.log(`Server has started on port ${PORT}`));
+server.listen(PORT, () => console.log(`Server has started on port ${PORT}`));
