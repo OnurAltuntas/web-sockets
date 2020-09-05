af15d96696cfbf12b206be4621a1981cc969db4c
Onur Altuntas <onuraltuntas50@gmail.com>
2020-09-05 21:29:28 +0300

    fix working directory

--- src/App.js
+++ src/App.js
@@ -25,7 +25,7 @@ export default class App extends Component {
   }
 
   start = () =>{
-    var socket = io('https://dude-its-a-game.herokuapp.com/');
+    var socket = io();
 
     if(!this.state.isGameRunning){
       this.game = new Game(this.getCtx(), socket);
